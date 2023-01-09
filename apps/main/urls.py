918d44b28f65c71681f6df84b96bc942ba9f783d
Lucas dos Santos <santoslucasantos796@gmail.com>
2023-01-09 00:09:21 -0300

    Criado urls para contato, sobre nós e registro

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import HomeView, LoginView
+from .views import HomeView, LoginView, AboutView, ContactView, RegisterView
 
 app_name = "main"
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("login", LoginView.as_view(), name="login"),
+    path("about", AboutView.as_view(), name="about"),
+    path("contact", ContactView.as_view(), name="contact"),
+    path("register", RegisterView.as_view(), name="register"),
 ]
